46573a835e9bd95c014c9d0e0e863e7eebe94c23
Borui <79346559+barikyo@users.noreply.github.com>
2026-01-12 18:15:58 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,15 +3,20 @@ using System.Windows.Forms;
 
 namespace CialloBrowser
 {
-    static class Program
+    internal static class Program
     {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
-            ApplicationConfiguration.Initialize();
-            // 启动 Form1
+            //修复：使用 Framework 4.8 的传统启动方式
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            // 启动主窗口
             Application.Run(new Form1());
         }
     }
-
 }
